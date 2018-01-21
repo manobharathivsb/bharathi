f24f736111ac280594ba7ca66ddc33fb4a525670
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-01-21 15:19:28 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) 
+{
+int base,exponent,i,power,e;
+scanf("%d",&base);
+printf("enter base\n");
+scanf("%d",&exponent);
+printf("enter exponent\n");
+power=base^exponent;
+printf("power:%d",power);
+	return 0;
+}
