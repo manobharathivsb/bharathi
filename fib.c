758ab200b82b5527d4ef7172dcbcf612aa8f9472
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-01-21 15:50:09 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int main(void)
 {
-int a=0,b=1;
-printf("%d%d",a,b);
-for(i=2;i<n;i++)
-{
+int a=0,b=1,i,n,c;
+printf("enter the number\n");
 scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+printf("%d",a);
 c=a+b;
-b=a;
-c=b;
-	return 0;
+a=b;
+b=c;
 }
