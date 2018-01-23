1efe481e7c88476d6198bcd38c843f611abb98b7
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-01-23 11:31:09 +0000

    Update digit.c

--- digit.c
+++ digit.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main(void) {
-	int n;
+int main(void) 
+{
+	int n,i;
 	scanf("%d",&n);
+	printf("enter the value\n");
+	for(i=0;i<=n;i++)
+	{
 	printf("%d",n);
+	}
 	return 0;
 }
