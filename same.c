45e329acfe1f9ccf25ec80e0df0e7865746840a7
manobharathivsb <31505612+manobharathivsb@users.noreply.github.com>
2018-02-08 14:48:51 +0530

    Update same.c

--- same.c
+++ same.c
@@ -1,12 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-	int a,i;
-	printf("enter the number");
-	scanf("%d",&a);
-	for(i=0;i<a;i++)
+	int x,y=0,z,n;
+	printf("\n enter the number");
+	scanf("%d",&n);
+	while(n!=0)
 	{
-		printf("%d\t",i);
+		x=n%10;
+		y=(y*10)+x;
+		n=n/10;
 	}
-return 0;
+	while(y!=0)
+	{
+		z=y%10;
+		printf("%d ",z);
+		y=y/10;
+	}
+	return 0;
 }
+
